21dd2dccf12fc42edf2ff9f275042eef0e331c09
codesleepcode <siddhartha_97@outlook.com>
2021-04-16 18:24:50 +1000

    changed few code

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,11 +2,13 @@
 
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+. ~/.nvm/nvm.sh
 nvm install node
+
 
 #create our working directory
 DIR = "/home/ec2-user/express-app"
-if [-d "$DIR"]; then
+if [- d "$DIR"]; then
     echo "${DIR} exists"
 else
     echo "creating ${DIR} directory"
